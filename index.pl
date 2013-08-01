30f863993ded53e53bebeb55bab0b3eae890cd24
DavidColblin <da-vid@hotmail.co.uk>
2013-08-01 13:18:08 +0400

    Created

---
+++ index.pl
@@ -0,0 +1,16 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+print "Content-Type: text/html\n\n";
+
+print "<HTML>\n";
+print "<HEAD>\n";
+print "<TITLE>Hello World</TITLE>\n";
+print "</HEAD>\n";
+print "<BODY>\n";
+print "<H4>Hello World</H4>\n";
+print "<P>\n";
+print "Your IP Address is $ENV{REMOTE_ADDR}.\n";
+print "<P>";
+print "<H5>Have a nice day</H5>\n";
+print "</BODY>\n";
+print "</HTML>\n"; 
